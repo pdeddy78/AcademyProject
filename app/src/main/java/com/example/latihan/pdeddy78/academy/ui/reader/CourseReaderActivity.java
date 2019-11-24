1275ab4c84897ea44d191ef0864443477ff59d4e
Deddy Pratama <pdeddy78@gmail.com>
2019-11-24 10:17:10 +0700

    implement CourseReaderCallback

--- app/src/main/java/com/example/latihan/pdeddy78/academy/ui/reader/CourseReaderActivity.java
+++ app/src/main/java/com/example/latihan/pdeddy78/academy/ui/reader/CourseReaderActivity.java
@@ -1,12 +1,14 @@
 package com.example.latihan.pdeddy78.academy.ui.reader;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 import com.example.latihan.pdeddy78.academy.R;
+import com.example.latihan.pdeddy78.academy.ui.reader.content.ModuleContentFragment;
 
-public class CourseReaderActivity extends AppCompatActivity {
+public class CourseReaderActivity extends AppCompatActivity implements CourseReaderCallback {
 
     public static final String EXTRA_COURSE_ID = "extra_course_ID";
 
@@ -14,5 +16,13 @@ public class CourseReaderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_reader);
+    }
+
+    @Override
+    public void moveTo(int position, String moduleId) {
+        Fragment fragment = ModuleContentFragment.newInstance();
+        getSupportFragmentManager().beginTransaction().add(R.id.frame_container, fragment, ModuleContentFragment.TAG)
+                .addToBackStack(null)
+                .commit();
     }
 }
