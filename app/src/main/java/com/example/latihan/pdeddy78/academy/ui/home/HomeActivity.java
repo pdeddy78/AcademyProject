350b5a2f4ecb6612bb3d0de4afc0f0f6db80d435
Deddy Pratama <pdeddy78@gmail.com>
2019-11-24 09:14:55 +0700

    connect instance Fragment in HomeActivity

--- app/src/main/java/com/example/latihan/pdeddy78/academy/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/latihan/pdeddy78/academy/ui/home/HomeActivity.java
@@ -1,37 +1,59 @@
 package com.example.latihan.pdeddy78.academy.ui.home;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.latihan.pdeddy78.academy.R;
+import com.example.latihan.pdeddy78.academy.ui.academy.AcademyFragment;
+import com.example.latihan.pdeddy78.academy.ui.bookmark.BookmarkFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+
+    private final String SELECTED_MENU = "selected_menu";
+    private BottomNavigationView navView;
+
+    Fragment fragment = null;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = menuItem -> {
         switch (menuItem.getItemId()) {
             case R.id.action_home:
-                mTextMessage.setText(R.string.home);
-                return true;
+                fragment = AcademyFragment.newInstance();
             case R.id.action_bookmark:
-                mTextMessage.setText(R.string.bookmark);
-                return true;
+                fragment = BookmarkFragment.newInstance();
         }
-        return false;
+
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .replace(R.id.container, fragment)
+                    .commit();
+        }
+        return true;
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        if (savedInstanceState != null) {
+            savedInstanceState.getInt(SELECTED_MENU);
+        } else {
+            navView.setSelectedItemId(R.id.action_home);
+        }
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SELECTED_MENU, navView.getSelectedItemId());
+    }
 }
