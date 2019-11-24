8c3526b97d05e8e075a5b5d7fb968fbfdcfbe5e3
Deddy Pratama <pdeddy78@gmail.com>
2019-11-24 08:59:00 +0700

    create BookmarkAdapter&Callback

---
+++ app/src/main/java/com/example/latihan/pdeddy78/academy/ui/bookmark/BookmarkFragmentCallback.java
@@ -0,0 +1,7 @@
+package com.example.latihan.pdeddy78.academy.ui.bookmark;
+
+import com.example.latihan.pdeddy78.academy.data.CourseEntity;
+
+interface BookmarkFragmentCallback {
+    void onShareClick(CourseEntity course);
+}
